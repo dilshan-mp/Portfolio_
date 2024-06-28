3dc6938de9eb11d4debd3d265ae501d00af451e9
IT21185298 <it21185298@my.sliit.lk>
2024-06-28 19:24:06 +0530

    background colors and font style setups

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,17 @@ const config = {
       lg:"968",
       xl:"1200px",
     },
+    fontFamily:{
+      primary:"var(--font-jetbrainsMono)",
+    },
     extend: {
+      colors: {
+        primary:"#1c1c22",
+        accent: {
+          DEFAULT:"#00ff99",
+          hover:"#00e187"
+        }
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
