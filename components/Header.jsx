d155f4d2d33632e3e021826737a688508b04a7f5
IT21185298 <it21185298@my.sliit.lk>
2024-06-28 23:53:32 +0530

    one link create in nav bar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
+import { Button } from './ui/button'
 
 const Header = () => {
   return (
-    <header>
-      header
+    <header className='py-8 xl:py-12 text-whit'>
+      <div className='container mx-auto'>
+        <Link href='/'>
+        <h1 className='text-4xl font-semibold'>
+           Dilshan<span className='text-accent'>.</span>
+        </h1>
+        </Link>
+        <Nav />
+      </div>
     </header>
   )
 }
