44aca3cc0ad175b78d910fff1ff2f66982310037
IT21185298 <it21185298@my.sliit.lk>
2024-06-29 16:02:02 +0530

    mobile nav page create and import react icon depenceis

---
+++ components/MobileNav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Sheet,SheetContent,SheetTriagger } from "@/components/ui/sheet"
+import { usePathname } from 'next/navigation';
+import Link from "next/link";
+import {CiMenuFries} from 'react-icons/ci'
+
+const MobileNav = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MobileNav
